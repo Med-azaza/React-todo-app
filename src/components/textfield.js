bb6d9811a2514999d3ccaa94b6e4947c18266b04
med azaza <hamaaz666@gmail.com>
2021-05-03 04:04:17 +0000

    press enter and text of

--- src/components/textfield.js
+++ src/components/textfield.js
@@ -10,10 +10,15 @@ const Textfield= (props)=>{
         props.setTodos([...props.todos,{text: props.textinput, completed: false, id: Math.random()*1000}]);
         props.setTextinput('');
     }
+    const enter=(e)=>{
+        if (e.keyCode===13){
+            document.querySelector('.addbtn').click();
+        }
+    }
     return (
         <div className="text">
             <button  onClick={submit} className='addbtn'>ADD</button>
-            <input value={props.textinput} onChange={takeinput} type="text"/>
+            <input value={props.textinput} onChange={takeinput} onKeyUp={enter} type="text"/>
         </div>
     )
 }
